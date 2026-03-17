f4b9149516e3699db0bc5550362886112da9b0e3
adamgoplo <adam.sociale@gmail.com>
2026-03-17 14:51:19 +0100

    dummy policy file

---
+++ part_B/dummy_policy.py
@@ -0,0 +1,15 @@
+"""
+Python code implementing a dummy policy that never turns on the ventilation nor any heater, and leaves
+everything up to the overrule controllers.
+"""
+
+
+def select_action(state):
+
+    HereAndNowActions = {
+        "HeatPowerRoom1": 0,   # No heating in room 1
+        "HeatPowerRoom2": 0,   # No heating in room 2
+        "VentilationON":  0    # Ventilation OFF
+    }
+
+    return HereAndNowActions
